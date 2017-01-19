30f1d7576a30ebd08ebe174ed6b665ad8c29bacf
Matthew Greenberg <fmgreenberg@gmail.com>
2017-01-19 16:10:33 -0700

    deleted redundant files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
 import { compile } from 'handlebars';
 import { Stamp } from './stamp.js';
 import { context, source } from './data.js';
 
 let template = compile(source);
-let elt = <Stamp template={template} context={context} />
+let elt = <Stamp template={template} context={context} />;
 
 ReactDOM.render(
   elt,
